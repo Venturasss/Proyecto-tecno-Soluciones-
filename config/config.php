11acf8ed35b91e30c3324b0588eec4c4506dc139
Venturasss <venturaluis250@gmail.com>
2026-06-09 16:56:51 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,13 +1,10 @@
 <?php
-
 declare(strict_types = 1);
 
-const APP_NAME = 'tecnoSoluciones S.A ';
-
-const BASE_URL = 'http://localhost:8000/'; 
-
-const DB_HOST = '127.0.0.1';
-const DB_NAME = 'tecnosoluciones';
-const DB_USER = 'root';
-const DB_PASS = '';
+const APP_NAME = 'tecnoSoluciones S.A';
+const BASE_URL = 'http://localhost:8000/';
+const DB_HOST = $_ENV['DB_HOST'] ?? '127.0.0.1';
+const DB_NAME = $_ENV['DB_NAME'] ?? 'tecnosoluciones';
+const DB_USER = $_ENV['DB_USER'] ?? 'root';
+const DB_PASS = $_ENV['DB_PASS'] ?? '';
 const DB_CHARSET = 'utf8mb4';
